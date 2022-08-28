eb07b888bafbad0f00841d3a77fdec266b3b9ccd
s8189882 <s8189882@gmail.com>
2022-08-28 23:56:26 +0530

    Add main - Display welcome message

---
+++ Address Book/src/address/book/AddressBook.java
@@ -0,0 +1,9 @@
+package address.book;
+
+public class AddressBook {
+	public static void main(String[] args) {
+	
+		System.out.println("Welcome to Address Book Program in Java!");
+		System.out.println("You can create Contacts in the Address Book with first/last names, address, city, state, zip, phone number and email and so on");
+	}
+}

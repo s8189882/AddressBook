751a02476645e423b0b16007d003c8e2b1e8f3ab
s8189882 <s8189882@gmail.com>
2022-08-30 15:21:52 +0530

    Add UC3 - Ability to edit existing contact person using their name

--- Address Book/src/address/book/AddressBook.java
+++ Address Book/src/address/book/AddressBook.java
@@ -3,6 +3,7 @@ package address.book;
 import java.util.Scanner;
 
 public class AddressBook {
+	static Scanner sc = new Scanner(System.in);
 	
 	public static void main(String[] args) {
 		AddressBook entry = new AddressBook();
@@ -14,8 +15,6 @@ public class AddressBook {
 	}
 	
 	public void addContact() {
-		Scanner sc = new Scanner(System.in);
-
 		System.out.println("\nCreating a new contact!");
 		System.out.print("Enter First Name :	");
 		String firstname = sc.next();
@@ -43,11 +42,76 @@ public class AddressBook {
 		address.setZip(zip);
 		newcontact.setAddress(address);
 		displayContact(newcontact);
-		
-		sc.close();
+		editContact(newcontact);
 	}
 	
 	public void displayContact(Contact contact) {
 		System.out.println(contact);
+	}
+	
+	public void editContact(Contact contact) {
+		int choice = 0;
+		while (choice < 1 || choice > 4) {
+			System.out.println("\nWhat would you like to update?");
+			System.out.println("1. Name");
+			System.out.println("2. Phone Number");
+			System.out.println("3. Email Id");
+			System.out.println("4. Address");
+			System.out.print("\nEnter your choice : ");
+			choice = sc.nextInt();
+			
+			if (!(choice >=1 && choice <= 4))
+				System.out.println("\nInvalid choice!\nPlease try again.\n");
+		}
+		
+		switch (choice) {
+		case 1 :
+			System.out.print("Enter the updated First Name :	");
+			String firstname = sc.next();
+			System.out.print("Enter the updated Last Name :	");
+			String lastname = sc.next();
+			contact.setFirstName(firstname);
+			contact.setLastName(lastname);
+			break;
+			
+		case 2 :
+			System.out.print("Enter the updated Phone Number :	");
+			String number = sc.next();
+			contact.setPhoneNumber(number);
+			break;
+			
+		case 3 :
+			System.out.print("Enter the updated Email Address :	");
+			String email = sc.next();
+			contact.setEmailID(email);
+			break;
+			
+		case 4 :
+			System.out.print("Enter the updated City :	");
+			String city = sc.next();
+			System.out.print("Enter the updated State :	");
+			String state = sc.next();
+			System.out.print("Enter the updated zip :	");
+			String zip = sc.next();
+			contact.address.setCity(city);
+			contact.address.setState(state);
+			contact.address.setZip(zip);
+			break;
+		}
+		
+		System.out.println("\nIs there anything else you'd like to update?");
+		System.out.print("Enter 'Yes' or 'No' : ");
+		char continueEdit = sc.next().charAt(0);
+		if (continueEdit == 'Y' || continueEdit == 'y') {
+			editContact(contact);
+
+		}
+		else if (continueEdit == 'N' || continueEdit == 'n') {
+			System.out.println("\n\nHere is the updated Address Book.");
+			displayContact(contact);
+		}
+		else {
+			System.out.println("\nInvalid Input.\nPlease try again!");
+		}
 	}
 }
